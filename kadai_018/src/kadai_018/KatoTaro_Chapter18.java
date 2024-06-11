0bb11f5444bae799b5f524d8f7a9f5c0329e7b7c
Kinoao0105 <167842158+Kinoao0105@users.noreply.github.com>
2024-06-11 16:42:17 +0900

    Add files via upload

---
+++ kadai_018/src/kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18  {
+
+	public KatoTaro_Chapter18(String familyName, String givenName, String address) {
+		super(familyName, givenName, address);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+	public void setGivenName() {
+		this.givenName = "太郎";
+	}
+
+	public void eachIntroduce() {
+	   	 System.out.println("私はJavaが得意ですです");
+	}
+	
+}
